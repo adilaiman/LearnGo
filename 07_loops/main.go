453868ed1d0788274e1db8dcbd5439c723c3153b
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2020-01-19 22:02:29 +0000

    Loops

---
+++ 07_loops/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// long method
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	fmt.Println("<--------------------------------------->")
+
+	// short method
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Number: %d\n", i)
+	}
+
+	fmt.Println("<--------------------------------------->")
+
+	// fizzbuzz
+	// every multiple of 3 output fizz
+	// every multiple of 5 output buzz
+	// every multiple of both output fizzbuzz
+	// up to range 100
+	for i := 0; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else if i%15 == 0 {
+			fmt.Println("fizzbuzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
