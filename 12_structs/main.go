41c47c6e93b9e3bce88a66610a0e8db4d514e0a1
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2020-01-20 19:37:13 +0000

    Structs

---
+++ 12_structs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// define person struct
+type Person struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
+// greeting method (value receiver) "getter"
+// p similar to "this"
+func (p Person) greet() string {
+	return "Hello from " + p.firstName + "."
+}
+
+// change age method (pointer receiver) "setter"
+func (p *Person) changeAge(newAge int) {
+	p.age = newAge
+}
+
+func main() {
+	// init person using struct
+	person1 := Person{firstName: "foo", lastName: "bar", age: 40}
+
+	// shorthand
+	person2 := Person{"fizz", "buzz", 30}
+
+	fmt.Println(person1)
+	fmt.Println(person2)
+	fmt.Println(person1.firstName)
+	fmt.Println(person2.age)
+
+	fmt.Println(person1.greet())
+
+	fmt.Println(person1.age)
+	person1.changeAge(25)
+	fmt.Println(person1.age)
+}
