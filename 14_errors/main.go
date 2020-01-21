dcac24e48db58206ceb496b16d0317f5da5d9098
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2020-01-21 20:38:31 +0000

    Errors

---
+++ 14_errors/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func sqrt(number float64) (result float64, err error) {
+	if number < 0 {
+		return 0, errors.New("cannot find the square of a negative number")
+	}
+	return math.Sqrt(number), nil
+}
+
+func main() {
+	fmt.Println(sqrt(-1))
+	fmt.Println(sqrt(16))
+}
